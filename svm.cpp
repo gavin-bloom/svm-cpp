5c7f9f5b3fa47b1a45a5dcf2a4a2275345c97228
Gavin Bloom <70165980+gavin-bloom@users.noreply.github.com>
2023-04-03 15:07:36 -0400

    comments

--- svm.cpp
+++ svm.cpp
@@ -10,12 +10,13 @@ private:
     vector<vector<T>> X; // feature vectors
     vector<T> y; // labels
     vector<T> alpha; // Lagrange multipliers
-    T b; // threshold
+    T b; // bias
     T C; // regularization parameter
     T tol; // numerical tolerance
     int max_iter; // maximum number of iterations
     T sigma; // parameter for Gaussian kernel
     
+    //Gaussian kernel
     T kernel(const vector<T>& x1, const vector<T>& x2) const {
         T dist_sq = 0;
         for (int i = 0; i < x1.size(); ++i) {
@@ -35,29 +36,33 @@ public:
         max_iter = max_iter_;
         sigma = sigma_;
         
-	//SMO
+	//SMO loop
         int iter = 0;
         while (iter < max_iter) {
             int num_changed_alphas = 0;
             for (int i = 0; i < X.size(); ++i) {
                 T f_i = b;
                 for (int j = 0; j < X.size(); ++j) {
-                    f_i += alpha[j] * y[j] * kernel(X[j], X[i]); //predicted output
+                    f_i += alpha[j] * y[j] * kernel(X[j], X[i]); //predicted output for sample i
                 }
-                T E_i = f_i - y[i]; //error
+                T E_i = f_i - y[i]; //error for sample i
+		//check if error violates Karush–Kuhn–Tucker conditions
                 if ((y[i] * E_i < -tol && alpha[i] < C) || (y[i] * E_i > tol && alpha[i] > 0)) {
-                    int j = i;
+                    //select random sample j and compute error
+		    int j = i;
                     while (j == i) {
                         j = rand() % X.size();
-                    }
+		    }
                     T f_j = b;
                     for (int k = 0; k < X.size(); ++k) {
                         f_j += alpha[k] * y[k] * kernel(X[k], X[j]);
                     }
                     T E_j = f_j - y[j];
+			
                     T alpha_i_old = alpha[i];
                     T alpha_j_old = alpha[j];
-                    T L, H; //low and high bounds for alpha coefficients
+			
+                    T L, H; //compute low and high bounds for alpha coefficients
                     if (y[i] != y[j]) {
                         L = max(0.0, alpha[j] - alpha[i]);
                         H = min(C, C + alpha[j] - alpha[i]);
@@ -68,17 +73,24 @@ public:
                     if (L == H) {
                         continue;
                     }
+		    
+	            //compute eta
                     T eta = 2 * kernel(X[i], X[j]) - kernel(X[i], X[i]) - kernel(X[j], X[j]);
                     if (eta >= 0) {
                         continue;
                     }
+		    
+	            //comput new alpha for sample j and check tolerance
                     alpha[j] -= y[j] * (E_i - E_j) / eta;
                     alpha[j] = min(max(alpha[j], L), H);
                     if (abs(alpha[j] - alpha_j_old) < tol) {
                         alpha[j] = alpha_j_old;
                         continue;
                     }
+			
+		    //compute new alpha for sample i
                     alpha[i] += y[i] * y[j] * (alpha_j_old - alpha[j]);
+		    //compute bias
                     T b1 = b - E_i - y[i] * (alpha[i] - alpha_i_old) * kernel(X[i], X[i]) - y[j] * (alpha[j] - alpha_j_old) * kernel(X[i], X[j]);
 					T b2 = b - E_j - y[i] * (alpha[i] - alpha_i_old) * kernel(X[i], X[j]) - y[j] * (alpha[j] - alpha_j_old) * kernel(X[j], X[j]);
                     if (alpha[i] > 0 && alpha[i] < C) {
@@ -88,9 +100,11 @@ public:
                     } else {
                         b = (b1 + b2) / 2;
                     }
+		   
                     ++num_changed_alphas;
                 }
             }
+            
             if (num_changed_alphas == 0) {
                 ++iter;
             } else {
